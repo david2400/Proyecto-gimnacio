67a2d6622a4095e88b6dfc3c0a701fd30842fd6f
David jaramillo lopez <davi42@hotmail.es>
2020-09-21 13:33:54 -0500

    Inicio Proyecto

---
+++ src/main/java/com/example/Gimnasio/ProyectoGimnasioApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gimnasio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGimnasioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGimnasioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Gimnasio/ProyectoGimnasioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gimnasio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGimnasioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
